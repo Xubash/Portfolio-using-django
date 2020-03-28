544833212a0270b576b080c4b33f6f9879e61958
Subash Manandhar <i11078@d2hs.com>
2020-03-28 19:23:26 +0545

    Setup Postgres Database

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Jobs(models.Model):
+    """
+    Attributes:
+        Image 
+        Summary
+    """
+    image =models.ImageField(upload_to='images/')
+    Summary=models.CharField(max_length=200)
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Jobs
 
 def home(request):
-    return render(request,'jobs/home.html')
+    jobs = Jobs.objects.all()
+    return render(request,'jobs/home.html',{'jobs':jobs})
 
