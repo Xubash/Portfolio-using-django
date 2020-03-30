4e4761e8bdd3a240bbe5dca528b4cb5a3d483464
Subash Manandhar <i11078@d2hs.com>
2020-03-30 10:47:35 +0545

    Commit for hosting

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lp^43-+^z5!8nm593eqgwc6oy6dcaybdn92w5ptmdr4!^9s!$h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
